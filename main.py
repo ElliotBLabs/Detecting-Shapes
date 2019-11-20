85180c1de2f4bafb60d0e84570625269568391be
ElliotBLabs <43539407+ElliotBLabs@users.noreply.github.com>
2019-11-20 10:01:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 # import the necessary packages
 import numpy as np
 import argparse
-import imutils
-import cv2_tools
-import pillow
+Pillow = __import__('PIL')
+
+
+cv2 = __import__('opencv--python')
+
+
+def grab_contours(cnts):
+    # if the length the contours tuple returned by cv2.findContours
+    # is '2' then we are using either OpenCV v2.4, v4-beta, or
+    # v4-official
+    if len(cnts) == 2:
+        cnts = cnts[0]
+
+    # if the length of the contours tuple is '3' then we are using
+    # either OpenCV v3, v4-pre, or v4-alpha
+    elif len(cnts) == 3:
+        cnts = cnts[1]
+
+    # otherwise OpenCV has changed their cv2.findContours return
+    # signature yet again and I have no idea WTH is going on
+    else:
+        raise Exception(("Contours tuple must have length 2 or 3, "
+            "otherwise OpenCV changed their cv2.findContours return "
+            "signature yet again. Refer to OpenCV's documentation "
+            "in that case"))
+
+    # return the actual contours array
+    return cnts
 
 """
 # construct the argument parse and parse the arguments
@@ -15,7 +40,7 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 """
 
-image = pillow.Image.open("ascii-drawing-symbol-5.png")
+image = PIL.Image.open("ascii-drawing-symbol-5.png")
 # find all the 'black' shapes in the image
 lower = np.array([0, 0, 0])
 upper = np.array([15, 15, 15])
